28a0873bd0c021a5c31925a4c012739b33bedabd
Gabriel C <gabriel.chaiix@gmail.fr>
2022-07-08 00:14:49 +0200

    maj sdk-identitystore

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
-const badgr = import("./badgr.js");
+const badgr = require("./badgr.js");
+const { IdentitystoreClient, DescribeUserCommand } = require("@aws-sdk/client-identitystore");
+const client = new IdentitystoreClient({ region: "eu-west-1" });
 
 exports.handler = async (event) => {
   return "ok";
 };
 
 (async () => {
-    let token = await badgr.getConnexionToken()
+    //let token = await getConnexionToken()
+    input = {
+        "IdentityStoreId": "d-93670b1d31",
+        "UserId": "93670b1d31-dcf6e9ac-0c04-4405-b751-a976fa599afa"
+     }
+    const command = new DescribeUserCommand(input);
+    const response = await client.send(command);
+    console.log(response)
+
+
     /*let userlist = await getUserAwardedList(
       "Bearer NkrpkHvxFduzS54jKSLEFXXe1CtBIy",
       "rFeAiLf4THyx1Zl0ugBA8A"
     );*/
     //console.log(userlist);
-    console.log(token)
+    //console.log(token)
   })();
 
